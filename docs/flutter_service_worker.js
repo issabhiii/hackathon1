73e421b20082832ddd2041caab53193ecd9afb93
issabhiii <abhinav.jain.9359@gmail.com>
2025-09-27 12:02:09 -0500

    Publish Flutter web to /docs (no Jekyll) + 404 fallback

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bce99f483b7fb9f5d2c41291c8743c93
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "4217bc56b634ebe424780ffc22d4cd8d",
+"flutter_bootstrap.js": "44ddf690cb686df670c5b0909f0451cb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
